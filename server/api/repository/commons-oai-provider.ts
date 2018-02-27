9e95e6ebca17d61b719d085b384f1240f9c91afc
Michael W Spalti <mspalti@gmail.com>
2018-02-27 15:46:51 -0800

    chore: Undated the copyright statement.
    Issues: AC-1236

--- server/api/repository/commons-oai-provider.ts
+++ server/api/repository/commons-oai-provider.ts
@@ -1,25 +1,24 @@
 /*
- * Copyright 2018 Willamette University
+ *  Copyright 2018 Willamette University
  *
- * This file is part of commons-oai-provider.
+ *  This file is part of tagger-oai-provider.
  *
- * commons-oai-provider is based on the Modular OAI-PMH Server, University of Helsinki, The National Library of Finland.
+ *  tagger-oai-provider is based on the Modular OAI-PMH Server, University of Helsinki,
+ *  The National Library of Finland.
  *
- *     commons-oai-provider is free software: you can redistribute it and/or modify
- *     it under the terms of the GNU General Public License as published by
- *     the Free Software Foundation, either version 3 of the License, or
- *     (at your option) any later version.
+ *  tagger-oai-provider is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
  *
- *     Foobar is distributed in the hope that it will be useful,
- *     but WITHOUT ANY WARRANTY; without even the implied warranty of
- *     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- *     GNU General Public License for more details.
+ *  tagger-oai-provider is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
  *
- *     You should have received a copy of the GNU General Public License
- *     along with commons-oai-provider.  If not, see <http://www.gnu.org/licenses/>.
+ *  You should have received a copy of the GNU General Public License
+ *  along with tagger-oai-provider.  If not, see <http://www.gnu.org/licenses/>.
  */
-
-/* eslint-disable no-unused-vars */
 
 /**
  * @typedef {Object} metadataFormat
@@ -47,28 +46,9 @@
  * @property {number} [cursor]
  */
 
-/**
- * @external {Document} https://developer.mozilla.org/en-US/docs/Web/API/Document
- */
-/**
- * @typedef {Object} record
- * @property {string} identifier
- * @property {Date} timestamp
- * @property {boolean} [deleted]
- * @property {Document} [metadata]
- * @property {string[]} [setSpec] - An array of {@link set} specs
- */
 
 import {MysqlConnector} from "../dao/mysql";
 
-/**
- * @typedef {Object} oaiService
- * @property {getCapabilities} getCapabilities - Get capabilities of the backend module
- * @property {getSets} getSets - Get the sets the backend supports
- * @property {getRecords} getRecords - xxx
- * @property {getIdentifiers} getIdentifiers - xxx
- * @property {getRecord} getRecord - xxx
- */
 export interface OaiProvider {
     getCapabilities: any;
     getSets: any;
