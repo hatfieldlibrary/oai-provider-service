1350a2ad9a780d2d75128feaced7a22e74976d92
Michael W Spalti <mspalti@gmail.com>
2018-02-28 15:51:16 -0800

    chore: Added sample-provider configuration file.

---
+++ server/providers/sample-provider/configuration.ts
@@ -0,0 +1,31 @@
+/*
+ *  Copyright 2018 Willamette University
+ *
+ *  This file is part of tagger-oai-provider.
+ *
+ *  @author Michael Spalti
+ *
+ *  tagger-oai-provider is based on the Modular OAI-PMH Server, University of Helsinki,
+ *  The National Library of Finland.
+ *
+ *  tagger-oai-provider is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+ *
+ *  tagger-oai-provider is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with tagger-oai-provider.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+
+
+// Not used by the sample-provider. See tagger provider for working example.
+// It's a good idea to place database configuration, etc., in an external
+// file (typically in /etc) that can be used to update the application via
+// configuration.  This also removes sensitive information like passwords
+// from the VCS.
