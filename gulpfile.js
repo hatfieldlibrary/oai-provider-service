a5b7225445d16cb7be46a72221b92eea5e05935c
Michael W Spalti <mspalti@gmail.com>
2018-02-28 15:53:36 -0800

    chore: Updated the copyright license.

--- gulpfile.js
+++ gulpfile.js
@@ -1,25 +1,25 @@
 /*
  *  Copyright 2018 Willamette University
  *
- *  This file is part of tagger-oai-provider.
- *
+ *  This file is part of OAI-PHM Service.
+ *  
  *  @author Michael Spalti
  *
- *  tagger-oai-provider is based on the Modular OAI-PMH Server, University of Helsinki,
+ *  OAI-PHM Service is based on the Modular OAI-PMH Server, University of Helsinki, 
  *  The National Library of Finland.
  *
- *  tagger-oai-provider is free software: you can redistribute it and/or modify
+ *  OAI-PHM Service is free software: you can redistribute it and/or modify
  *  it under the terms of the GNU General Public License as published by
  *  the Free Software Foundation, either version 3 of the License, or
  *  (at your option) any later version.
  *
- *  tagger-oai-provider is distributed in the hope that it will be useful,
- *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  OAI-PHM Service is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of 
  *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  *  GNU General Public License for more details.
  *
  *  You should have received a copy of the GNU General Public License
- *  along with tagger-oai-provider.  If not, see <http://www.gnu.org/licenses/>.
+ *  along with OAI-PHM Service.  If not, see <http://www.gnu.org/licenses/>.
  */
 
 var gulp = require("gulp");
